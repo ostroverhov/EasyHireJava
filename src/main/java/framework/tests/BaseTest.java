9bc9dd786a5cec7fe1810a1da2649fcf45672cee
U.Astraverkhau <U.Astraverkhau@itechart-group.com>
2021-08-31 17:13:43 +0300

    fix allure

--- src/main/java/framework/tests/BaseTest.java
+++ src/main/java/framework/tests/BaseTest.java
@@ -2,12 +2,9 @@ package framework.tests;
 
 import framework.drivers.BrowserFactory;
 import framework.utils.Logger;
-import io.qameta.allure.Attachment;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestInfo;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 
 public class BaseTest {
 
@@ -15,7 +12,7 @@ public class BaseTest {
     protected final BrowserFactory browserFactory = new BrowserFactory();
 
     @BeforeEach
-    public void setUpBrowser(TestInfo testInfo){
+    public void setUpBrowser(TestInfo testInfo) {
         logger.info("Start scenario " + testInfo.getDisplayName());
         BrowserFactory.setUrl(browserFactory.getDriver());
     }
@@ -23,12 +20,6 @@ public class BaseTest {
     @AfterEach
     public void tearDown(TestInfo testInfo) {
         logger.info("Finish test scenario " + testInfo.getDisplayName());
-        screenshot();
         BrowserFactory.closeBrowser(browserFactory.getDriver());
-    }
-
-    @Attachment(value = "Screenshot", type = "image/png")
-    public byte[] screenshot() {
-        return ((TakesScreenshot) browserFactory.getDriver()).getScreenshotAs(OutputType.BYTES);
     }
 }
